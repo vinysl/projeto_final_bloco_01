0392816fab159fcd4d0c6c795b85eea666e41552
Vinicius Lima <vinicius.slima99@icloud.com>
2023-11-27 16:24:04 -0300

    task2

--- src/ecommerce/Menu.java
+++ src/ecommerce/Menu.java
@@ -8,17 +8,24 @@ public class Menu {
 
 	public static void main(String[] args) {
 		Scanner leia = new Scanner(System.in);
-		int opcao=0;
+		int opcao=0, tipoCafe;
+		String nomeCafe, cafeTorrado, cafeMoido;
 		
 		while(true) {
 			
 			System.out.println("                                                                                 ");
 			System.out.println("BEM VINDO AO AROMA CELESTIAL: A SUA LOJA ESPECIALIZADA EM CAFÉS DE TODOS OS TIPOS");
 			System.out.println("                                                                                 ");
-			System.out.println("                    1 - Comprar café                                             ");
-			System.out.println("                    2 - Comprar acessórios                                       ");
-			System.out.println("                    3 - Processo de fabricação do café                           ");
-			System.out.println("                    4 - Conheça a nossa história                                 ");
+			System.out.println("                    1 - Cadastrar café                                           ");
+			System.out.println("                    2 - Listar café                                              ");
+			System.out.println("                    3 - Buscar café                                              ");
+			System.out.println("                    4 - Atualizar café                                           ");
+			System.out.println("                    5 - Apagar café                                              ");
+			System.out.println("                    6 - Comprar café                                             ");
+			System.out.println("                    7 - Comprar acessórios                                       ");
+			System.out.println("                    8 - Processo de fabricação do café                           ");
+			System.out.println("                    9 - Conheça a nossa história                                 ");
+			System.out.println("                    10 - Sair                                                     ");
 			System.out.println("                                                                                 ");
 			System.out.println("                    Entre com a opção desejada:                                  ");
 			
@@ -30,28 +37,68 @@ public class Menu {
 				opcao = 0;
 			}
 			
-			if (opcao == 3) {
+			if (opcao == 7) {
 				producao();
 				keyPress();
 			}
 			
-			if (opcao == 4) {
+			if (opcao == 8) {
 				sobre();
 				keyPress();
 			}
 			
+			if (opcao == 10) {
+				sair();
+				leia.close();
+				System.exit(0);
+			}
+			
 			switch (opcao) {
 			case 1:
-				System.out.println("\nComprar café");
+				System.out.println("\nCadastrar café");
+				
 				
 				keyPress();
 				break;
 				
 			case 2:
-				System.out.println("\nComprar acessórios");
+				System.out.println("\nListar café");
 				
 				keyPress();
 				break;
+				
+			case 3:
+				System.out.println("\nBuscar café");
+				
+				keyPress();
+				break;
+				
+			case 4:
+				System.out.println("\nAtualizar café");
+				
+				keyPress();
+				break;
+				
+			case 5:
+				System.out.println("\nApagar café");
+				
+				keyPress();
+				break;
+				
+			case 6:
+				System.out.println("\nComprar café");
+				
+				keyPress();
+				break;
+				
+			case 7:
+				System.out.println("Comprar acessórios");
+				
+				keyPress();
+				break;
+				default:
+					System.out.println("\nDigite uma opção válida...");
+					keyPress();
 			}
 		}
 
@@ -69,6 +116,10 @@ public class Menu {
 		System.out.println("\nA Café Celestial é dedicada à produção de café de alta qualidade. "
 				+ "Grãos cuidadosamente selecionados de fazendas sustentáveis passam por um processo meticuloso, desde a colheita até a torra artesanal. "
 				+ "A habilidade dos baristas em preparar o café destaca-se, proporcionando aos clientes uma experiência celestial em cada xícara.");
+	}
+	
+	public static void sair() {
+		System.out.println("\nObrigado por acessar a Aroma Celestial! :)");
 	}
 	
 	public static void keyPress() {
